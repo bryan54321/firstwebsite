a762ae572f13dc41b469e1b0de4ad2bd8eac91d5
bryan dominguez <110932246+bryan54321@users.noreply.github.com>
2024-09-12 07:48:09 -0500

    Update operators.php

---
+++ operators.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    //string
+    $name = 'tennis is fun';
+
+    //Integer
+    $name = 20;
+
+    //Float
+    $name = 20.5784;
+
+    //Boolean
+
+
+    //Array
+    $names = array("chayce","ben","carlos");
+    echo $names['2'];
+?>
+
+</body>
+</html>
