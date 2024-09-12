e9155bf02b21f44b278fe3403bb6fd345312233a
bryan dominguez <110932246+bryan54321@users.noreply.github.com>
2024-09-12 08:40:59 -0500

    Create conditionals.php

---
+++ conditionals.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $x = 4;
+
+        if ($x == 1){
+            echo "tennis is very fun";
+        } 
+        elseif  ($x == 2){
+            echo "tennis sucks";
+        }
+        elseif ($x == 3) {
+            echo "tennis is a very difficult sport";
+        }
+        elseif ($x == 4) {
+            echo "i am good at tennis";
+        }
+        else {
+            echo "ive never played tennis";
+        }
+    ?>
+
+
+video 14
+
+<?php
+
+
+?>
+
+
+
+</body>
+</html>
