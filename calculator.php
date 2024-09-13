b1e2bcbba5702869e0ba3c985722d779c9e78db0
bryan54321 <bryan.dominguez@students.jmcss.org>
2024-09-12 19:28:39 -0700

    Create calculator.php

---
+++ calculator.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form>
+    <input type="text" name="num1" placeholder="number 1">
+    <input type="text" name="num2" placeholder="number 2">
+</form>
+
+</body>
+</html>
