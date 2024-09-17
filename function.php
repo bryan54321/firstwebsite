abb75b9f3985b048ffc3254d8af4d3091804bc94
bryan dominguez <110932246+bryan54321@users.noreply.github.com>
2024-09-17 08:42:33 -0500

    coding

---
+++ function.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+    function newCalc($x) {
+        $newnr = $x * 0.75;
+        echo "here is 75% of what you wrote: ".$newnr;
+    }
+
+
+$x = 100;
+newCalc($x);
+echo "<br>";
+$a = 10;
+newCalc($a);
+
+
+
+?>
+
+
+
+
+</body>
+</html>
--- loops.php
+++ loops.php
@@ -7,5 +7,43 @@
 </head>
 <body>
     
+<?php
+    //While loop
+    $x = 1;
+    while ($x <= 5) {
+        echo "Coach B is a good Teacher<br>";
+        $x++;
+    }
+?>
+<br>
+<?php
+    //Do while loop
+    $x = 1;
+    do {
+        echo "he makes coding fun<br>";
+        $x++;
+    }
+    while ($x <= 5);
+?>
+<br>
+<?php
+    //For loop
+    for ($x = 1; $x <= 10; $x++) {
+        echo "Hows your day been? <br>";
+    }
+
+
+?>
+<br>
+<?php
+    //Foreach loop
+    $array = array ("good", "bad", "okay");
+
+    foreach ($array as $loopdata) {
+        echo "My day has been ".$loopdata. "<br>";
+    }
+
+?>
+
 </body>
 </html>
